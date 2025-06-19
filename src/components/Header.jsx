03e2aff54bc8f14cbd917bf44f3c9592eabf1f8f
David Marongiu <davidmarongiu@hotmail.com>
2025-06-19 15:29:23 +0200

    feature: add text hello in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import image from '../assets/investment-calculator-logo.png';
 
 const Header = () => {
+
     return (
         <header id="header">
             <img src={image} alt="Logo showing a money bag"/>
+            <p>hello </p>
             <h1>Investment Calculator</h1>
         </header>
     );
